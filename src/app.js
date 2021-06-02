667efc214d3448e27796dae1a7754c9e35884d1d
Cristian Fernando García Bernabe <crisarmado@hotmail.com>
2021-06-02 10:04:11 -0500

    carpeta del código y primer archivo

---
+++ src/app.js
@@ -0,0 +1,26 @@
+//servidor en todo este archivo
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+//conectar a la BD,si no existe se crea automaticamente
+mongoose.connect('mongodb://localhost/pelisplusplus').then(db=>console.log('conexión a la BD'+db+ "realizada correctamente"))
+.catch(err=>console.error("Ha ocurrido un error: "+err));
+//importar rutas
+const indexRoutes = require('./routes/index');
+//const { Console } = require('console');
+//configuraciones
+app.set('port',process.env.PORT || 3000);//arrancar en el puerto que asigna el SO o el 3000 si no hay
+app.set('views',path.join(__dirname,'views'));//Join para que no importe SO del server
+app.set('view engine','ejs');
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));//enteder solicitudes que mandan, extended:false porque no se enviaran archivos grandes
+//rutas
+app.use('/',indexRoutes);
+//empezando servidor
+app.listen(app.get('port'), () =>{
+console.log(`Server on port ${app.get('port')}`);//`= ALTGR + }
+});
